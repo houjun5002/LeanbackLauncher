a05599f706a42c55d82ca8845852fe375c29c42d
user9246400285 <1558467779761449-user9246400285@noreply.coze.cn>
2026-03-25 14:19:42 +0800

    fix: 添加缺失的 okhttp3 import
    添加 import okhttp3.RequestBody.Companion.toRequestBody

--- app/src/main/java/com/amazon/tv/leanbacklauncher/MainActivity.kt
+++ app/src/main/java/com/amazon/tv/leanbacklauncher/MainActivity.kt
@@ -120,6 +120,7 @@ import okhttp3.MultipartBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
 import java.io.File
 import java.io.FileDescriptor
